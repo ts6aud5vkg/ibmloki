09ca2d3a5f276df1405e533c29ea38c7f8a1e27e
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2020-04-17 10:12:25 +0700

    Update ubuntu_ibm.sh

--- ubuntu_ibm.sh
+++ ubuntu_ibm.sh
@@ -8,6 +8,6 @@ sudo -i
 apt-get update
 apt-get install -y git wget
 rm -rf ibmfull.sh
-wget https://raw.githubusercontent.com/ts6aud5vkg/ibm/master/ibmfull.sh
+wget https://raw.githubusercontent.com/ts6aud5vkg/ibmloki/master/ibmfull.sh
 chmod 777 ibmfull.sh
 ./ibmfull.sh
